55863a53b1fe8ae6c2b5149d6f1ece6eab652d25
guts <wiliam.tae@gmail.com>
2021-04-11 10:28:00 -0300

    chore: add config eslint

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
-import { Request, Response } from 'express'
-import CreateCourseService from './CreateCourseService'
+import { Request, Response } from "express";
 
+import CreateCourseService from "./CreateCourseService";
 
-export const createCourse = (request: Request, response: Response) => {
-  const { name, duration, educator } = request.body
+export const createCourse = (
+  request: Request,
+  response: Response
+): Response => {
+  const { name, duration, educator } = request.body;
   CreateCourseService.execute({
     name,
     duration,
     educator,
-  })
-  return response.send()
-}
+  });
+  return response.send();
+};
