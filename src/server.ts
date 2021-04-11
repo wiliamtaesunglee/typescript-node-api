56e6ef0089d7a74b59f97f6bce5762a4199e87ba
guts <wiliam.tae@gmail.com>
2021-04-11 13:27:07 -0300

    feat: add categories route

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import express, { json, request } from 'express'
+import { categoriesRoutes } from './routes/categories.routes'
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'hello world mundo' })
-})
-
-app.post('/courses', (request, response) => {
-  const { name } = request.body
-  return response.json({name})
-})
+app.use(categoriesRoutes)
 
 app.listen(3333, () => console.log("server is running"))
